76c8a02616b6c436ba25772515b137bc1e348907
leeminjun98 <minj4634@gmail.com>
2023-10-14 20:30:05 +0900

    branchmerge

--- src/main/java/myown/firstpractice/controller/HelloController.java
+++ src/main/java/myown/firstpractice/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
     @GetMapping
-    public String hello(Model model){
+    public String hello(Model model){//주석
         model.addAttribute("data","spring!!");
         return "hello";
     }
